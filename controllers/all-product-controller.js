61965893793248c44174b21a8e64f47eaaa1cce3
Wenceslao Janda <wenchojanda@gmail.com>
2022-05-05 17:43:52 -0300

    hosting a json-server

--- controllers/all-product-controller.js
+++ controllers/all-product-controller.js
@@ -10,7 +10,7 @@ productServices.listaProductos().then((data) => {
                         <a class="delete-icons" href="id=${producto.id}"><i class="fa-solid fa-trash"></i></a>
                         <a class="edit-icons" href="./edit-product.html?id=${producto.id}"><i class="fa-solid fa-pencil"></i></a>
                     </div>
-                    <img class="products-img" src="${producto.imagen}" alt="star wars image">
+                    <img class="products-img" src="../assets/images/section/${producto.imagen}" alt="star wars image">
                     <h4>${producto.nombre}</h4>
                     <p class="products-price">R$${producto.precio}</p>
                     <p>#${producto.id}</p>
@@ -21,5 +21,3 @@ productServices.listaProductos().then((data) => {
    productsContainer.innerHTML = cards;
 
 })
-
-//../assets/images/section/
